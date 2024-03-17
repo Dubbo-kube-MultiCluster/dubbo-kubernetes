d3ab8d1a1c0450e48b2b7b4e7e3fd815cc786ee9
wuyan-dushang <1550693033@qq.com>
2024-03-17 13:05:43 +0800

    feat: mapping and metadata create

--- pkg/plugins/resources/traditional/store.go
+++ pkg/plugins/resources/traditional/store.go
@@ -19,11 +19,22 @@ package traditional
 
 import (
 	"context"
+	"dubbo.apache.org/dubbo-go/v3/common"
 	"dubbo.apache.org/dubbo-go/v3/config_center"
+	dubbo_identifier "dubbo.apache.org/dubbo-go/v3/metadata/identifier"
 	"dubbo.apache.org/dubbo-go/v3/metadata/report"
 	dubboRegistry "dubbo.apache.org/dubbo-go/v3/registry"
+	mesh_proto "github.com/apache/dubbo-kubernetes/api/mesh/v1alpha1"
+	"github.com/apache/dubbo-kubernetes/pkg/core/resources/apis/mesh"
 	core_model "github.com/apache/dubbo-kubernetes/pkg/core/resources/model"
 	"github.com/apache/dubbo-kubernetes/pkg/core/resources/store"
+	"golang.org/x/exp/maps"
+)
+
+const (
+	dubboGroup   = "dubbo"
+	mappingGroup = "mapping"
+	cpGroup      = "dubbo-cp"
 )
 
 type traditionalStore struct {
@@ -45,6 +56,71 @@ func NewStore(
 }
 
 func (t *traditionalStore) Create(_ context.Context, resource core_model.Resource, fs ...store.CreateOptionsFunc) error {
+	var err error
+	opts := store.NewCreateOptions(fs...)
+
+	switch resource.Descriptor().Name {
+	case mesh.MappingType:
+		spec := resource.GetSpec()
+		mapping := spec.(*mesh_proto.Mapping)
+		appNames := mapping.ApplicationNames
+		serviceInterface := mapping.InterfaceName
+		for _, app := range appNames {
+			err = t.metadataReport.RegisterServiceAppMapping(serviceInterface, mappingGroup, app)
+			if err != nil {
+				return err
+			}
+		}
+	case mesh.MetaDataType:
+		spec := resource.GetSpec()
+		metadata := spec.(*mesh_proto.MetaData)
+		identifier := &dubbo_identifier.SubscriberMetadataIdentifier{
+			Revision: metadata.GetRevision(),
+			BaseApplicationMetadataIdentifier: dubbo_identifier.BaseApplicationMetadataIdentifier{
+				Application: metadata.GetApp(),
+				Group:       dubboGroup,
+			},
+		}
+		services := map[string]*common.ServiceInfo{}
+		// 把metadata赋值到services中
+		for key, serviceInfo := range metadata.GetServices() {
+			services[key] = &common.ServiceInfo{
+				Name:     serviceInfo.GetName(),
+				Group:    serviceInfo.GetGroup(),
+				Version:  serviceInfo.GetVersion(),
+				Protocol: serviceInfo.GetProtocol(),
+				Path:     serviceInfo.GetPath(),
+				Params:   serviceInfo.GetParams(),
+			}
+		}
+		info := &common.MetadataInfo{
+			App:      metadata.GetApp(),
+			Revision: metadata.GetRevision(),
+			Services: services,
+		}
+		err = t.metadataReport.PublishAppMetadata(identifier, info)
+		if err != nil {
+			return err
+		}
+	case mesh.DataplaneType:
+
+	case mesh.TagRouteType:
+
+	case mesh.ConditionRouteType:
+
+	case mesh.DynamicConfigType:
+
+	default:
+
+	}
+
+	resource.SetMeta(&resourceMetaObject{
+		Name:             opts.Name,
+		Mesh:             opts.Mesh,
+		CreationTime:     opts.CreationTime,
+		ModificationTime: opts.CreationTime,
+		Labels:           maps.Clone(opts.Labels),
+	})
 	return nil
 }
 
